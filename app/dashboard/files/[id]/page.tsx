bd4b25e32bcf088109349c5809c25d9b6a0a058c
John Gill <John.Gill@anthill.co.uk>
2024-09-10 11:49:34 +0100

    commit code to view the pdf

--- app/dashboard/files/[id]/page.tsx
+++ app/dashboard/files/[id]/page.tsx
@@ -1,11 +1,41 @@
-function ChatToFilePage({
+import { adminDb } from "@/firebaseAdmin";
+import { auth } from "@clerk/nextjs/server";
+import PdfView from "@/components/PdfView";
+
+async function ChatToFilePage({
   params: { id },
 }: {
   params: {
     id: string;
   };
 }) {
-  return <div>ChatToFilePage: {id}</div>;
+  auth().protect();
+  const { userId } = await auth();
+
+  const ref = await adminDb
+    .collection("users")
+    .doc(userId!)
+    .collection("files")
+    .doc(id)
+    .get();
+
+  const url = ref.data()?.downloadUrl;
+
+  return (
+    <div className="grid lg:grid-cols-5 h-full overflow-hidden">
+      {/* right */}
+
+      <div className="col-span-5 lg:col-span-2 overflow-y-auto">
+        {/* Chat */}
+      </div>
+
+      {/* LEFT */}
+      <div className="col-span-5 lg:col-span-3 bg-gray-100 border-r-2 lg:border-indigo-600 lg:-order-1 overflow-auto ">
+        {/* PDFView */}
+        <PdfView url={url} />
+      </div>
+    </div>
+  );
 }
 
 export default ChatToFilePage;
